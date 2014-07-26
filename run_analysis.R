e63a250bca03afca416a98d54e5781836e60bc50
Wei-Meng Lee <weimeng@staticfusion.org>
2014-07-27 02:18:34 +0800

    Add comments

--- run_analysis.R
+++ run_analysis.R
@@ -5,16 +5,19 @@
 # Note: This step assumes that the project data has been downloaded and
 # extracted to the "./UCI HAR Dataset" directory.
 
+# Prepare test data
 testSubjects <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 testActivities <- read.table("./UCI HAR Dataset/test/y_test.txt")
 testData <- read.table("./UCI HAR Dataset/test/X_test.txt")
 testSet <- cbind(testSubjects, testActivities, testData)
 
+# Prepare training data
 trainingSubjects <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 trainingActivities <- read.table("./UCI HAR Dataset/train/y_train.txt")
 trainingData <- read.table("./UCI HAR Dataset/train/X_train.txt")
 trainingSet <- cbind(trainingSubjects, trainingActivities, trainingData)
 
+# Merge test and training data
 totalSet <- rbind(testSet, trainingSet)
 
 #
@@ -23,16 +26,39 @@ totalSet <- rbind(testSet, trainingSet)
 # measurement.
 #
 
+# Read data labels so we know which columns to extract
 dataLabels <- read.table("./UCI HAR Dataset/features.txt")$V2
+
+# Fix typo present in original data set. Please see:
 # https://class.coursera.org/getdata-005/forum/thread?thread_id=23#comment-753
 dataLabels <- gsub("BodyBody", "Body", dataLabels)
+
+# Remove brackets
 dataLabels <- gsub("\\(\\)", "", dataLabels)
+
+# Add labels for Subject and Activity columns
 dataLabels <- append(c("Subject", "Activity"), dataLabels)
+
+# Apply labels to combined data set
 names(totalSet) <- dataLabels
 
-wantedColumns <- grep("-mean-|-mean$|std", names(totalSet))
+# Determine the columns which we want. Explanation of regular expressions:
+
+# Get standard deviations:
+#   std    => Match columns whose name contains "std".
+
+# Get means:
+#   -mean- => Match columns whose name contains "-mean-"
+#   -mean$ => Match columns whose name ends with "-mean"
+#
+#   Note: These two regexes are used to avoid selecting "meanFreq", which should
+#   be excluded from the data set. Please see the following link for discussion:
+#   https://class.coursera.org/getdata-005/forum/thread?thread_id=180#comment-568
+
+wantedColumns <- grep("std|-mean-|-mean$", names(totalSet))
 wantedColumns <- append(c(1, 2), wantedColumns)
 
+# Apply subset to get only mean and standard deviations
 wantedSet <- totalSet[wantedColumns]
 
 #
@@ -40,8 +66,13 @@ wantedSet <- totalSet[wantedColumns]
 # Use descriptive activity names to name the activities in the data set
 #
 
+# Convert activity values to factors
 wantedSet$Activity <- as.factor(wantedSet$Activity)
+
+# Get activity names
 activityLabels <- read.table("./UCI HAR Dataset/activity_labels.txt")$V2
+
+# Apply activity names to factor levels
 levels(wantedSet$Activity) <- activityLabels
 
 #
@@ -49,12 +80,15 @@ levels(wantedSet$Activity) <- activityLabels
 # Appropriately label the data set with descriptive variable names.
 #
 
+# Make it clearer which domain the measurements are made in
 names(wantedSet) <- gsub("^t", "Time", names(wantedSet))
 names(wantedSet) <- gsub("^f", "Frequency", names(wantedSet))
 
+# Make it clearer what the source of the measurements were
 names(wantedSet) <- gsub("Acc", "Accelerometer", names(wantedSet))
 names(wantedSet) <- gsub("Gyro", "Gyrometer", names(wantedSet))
 
+# Make cases consistent with rest of label
 names(wantedSet) <- gsub("mean", "Mean", names(wantedSet))
 names(wantedSet) <- gsub("std", "Std", names(wantedSet))
 
@@ -66,13 +100,15 @@ names(wantedSet) <- gsub("std", "Std", names(wantedSet))
 
 library(reshape2)
 
+# Melt and recast our data
 wantedMelt <- melt(wantedSet, id = c("Subject", "Activity"),
                            measure.vars = names(wantedSet)[-c(1, 2)])
-
 tidySet <- dcast(wantedMelt, Subject + Activity ~ variable, mean)
 
-
+# Check if tidydata.csv exists. If it does, delete it.
 if (file.exists("./tidydata.csv")) { unlink("./tidydata.csv") }
+
+# Write tidy data to tidydata.csv file
 tidyData <- file("./tidydata.csv", "w")
 write.csv(tidySet, file=tidyData)
 close(tidyData)
