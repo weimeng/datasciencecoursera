9941ba3f4c9c6a1b581bb0276a4862d60473b5f9
Wei-Meng Lee <weimeng@staticfusion.org>
2014-12-21 20:26:58 +0800

    Add additional information

--- app/ui.R
+++ app/ui.R
@@ -36,15 +36,19 @@ shinyUI(fluidPage(
 
     mainPanel(
       h2("Instructions"),
+      p("Hello amateur botanists! You've found yourself an iris but are unsure which species it is. Here's where this Shiny app can help."),
       tags$ol(
         tags$li("Enter the petal and sepal measurements of your iris species in the left panel"),
-        tags$li("Wait for the server to perform prediction"),
+        tags$li("Wait for the server to perform identification"),
         tags$li("View your results below")),
       h2("Results"),
-      tags$p("Based on the measurements you've entered on the left, your iris species is ",
+      tags$p("Based on the measurements you've entered on the left, your specimen is most likely to be ",
              tags$strong("Iris ",
                          textOutput("prediction", inline = TRUE)),
-             ".")
+             "."),
+      h2("Identification Information"),
+      p("The data set used to train the identification/prediction model is Edgar Anderson's Iris Data."),
+      p("The train() function of the caret package in R was used with default parameters to construct the prediction model.")
     )
   )
 ))
