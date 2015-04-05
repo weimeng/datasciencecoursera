412252109475b1d57c5e4f166cc48d288b3c5d9b
Wei-Meng Lee <weimeng@staticfusion.org>
2015-04-05 21:03:24 +0800

    Optimise cleanup and remove profanity

--- 10-dsscapstone-003/predict.R
+++ 10-dsscapstone-003/predict.R
@@ -8,6 +8,7 @@ options(mc.cores=1)
 twitter.sample.filename <- "./data/sample/en_US/en_US.twitter.txt"
 blogs.sample.filename <- "./data/sample/en_US/en_US.blogs.txt"
 news.sample.filename <- "./data/sample/en_US/en_US.news.txt"
+profanity.filename <- "./data/profanity/shutterstock_en.txt"
 
 # Read blogs sample data
 blogs.data.con <- file(blogs.sample.filename, "r")
@@ -24,20 +25,26 @@ twitter.data.con <- file(twitter.sample.filename, "r")
 twitter <- readLines(twitter.data.con)
 close(twitter.data.con)
 
+# Read profanity data
+profanity.con <- file(profanity.filename, "r")
+profanity <- readLines(profanity.con)
+close(profanity.con)
+
 # Combine all sample data
 all <- c(blogs, news, twitter)
 
-corpusCleanup <- function(corpus) {
-  corpus <- tm_map(corpus, content_transformer(iconv), from = "latin1", to = "ASCII", sub = "")
-  corpus <- tm_map(corpus, content_transformer(tolower))
-  corpus <- tm_map(corpus, removePunctuation)
-  corpus <- tm_map(corpus, stripWhitespace)
-  corpus <- tm_map(corpus, removeNumbers)
-  return(corpus)
+characterCleanup <- function(character) {
+  character <- iconv(character, from = "latin1", to = "ASCII", sub = "")
+  character <- tolower(character)
+  character <- removePunctuation(character)
+  character <- removeNumbers(character)
+  character <- removeWords(character, profanity)
+  character <- stripWhitespace(character)
 }
 
-all.corpus <- Corpus(VectorSource(all))
-all.corpus <- corpusCleanup(all.corpus)
+all.sanitized <- characterCleanup(all)
+
+all.corpus <- Corpus(VectorSource(all.sanitized))
 
 UnigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 1, max = 1))
 BigramTokenizer <- function(x) NGramTokenizer(x, Weka_control(min = 2, max = 2))
@@ -47,4 +54,6 @@ all.unigrams <- TermDocumentMatrix(all.corpus, control = list(tokenize = Unigram
 all.bigrams <- TermDocumentMatrix(all.corpus, control = list(tokenize = BigramTokenizer))
 all.trigrams <- TermDocumentMatrix(all.corpus, control = list(tokenize = TrigramTokenizer))
 
+all.unigrams.freq <- sort(row_sums(all.unigrams), decreasing = TRUE)
+all.bigrams.freq <- sort(row_sums(all.bigrams), decreasing = TRUE)
 all.trigrams.freq <- sort(row_sums(all.trigrams), decreasing = TRUE)
